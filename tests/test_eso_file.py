77e8ee9ada1759fce855e235738b6916fb37f6d1
voightp <vojtechpanek@seznam.cz>
2020-12-01 08:00:13 +0000

    set global eso file path

--- tests/test_eso_file.py
+++ tests/test_eso_file.py
@@ -4,18 +4,16 @@ import unittest
 from db_esofile_reader import DBEsoFile, DBEsoFileCollection
 from db_esofile_reader.constants import *
 
-TEST_FILES_DIR = os.path.join(os.path.dirname(__file__), "test_files")
+ESO_PATH = os.path.join(os.path.dirname(__file__), "test_files", "eplusout.eso")
 
 
 class TestEsofileReader(unittest.TestCase):
     def test_process_eso_file(self):
-        file = DBEsoFile.from_path(os.path.join(TEST_FILES_DIR, "eplusout.eso"))
+        file = DBEsoFile.from_path(ESO_PATH)
         expected = [H, D, M, RP]
         self.assertListEqual(expected, file.frequencies)
 
     def test_process_eso_file_collection(self):
-        files = DBEsoFileCollection.from_path(
-            os.path.join(TEST_FILES_DIR, "eplusout.eso")
-        )
+        files = DBEsoFileCollection.from_path(ESO_PATH)
         expected = ["UNTITLED (01-01:31-12)"]
         self.assertListEqual(expected, [f.environment_name for f in files])
